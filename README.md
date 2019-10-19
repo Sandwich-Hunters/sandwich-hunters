ca72cbf714c0dcff1d3817775d24d740ef0f0d8d
Ian Be <ian84be@gmail.com>
2019-10-19 15:28:34 -0500

    add css reset

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Sandwich-Hunters
-Sandwich Hunters
+* [Christopher J Foster](https://christopherjfoster.dev/)
+
+* [Ian Belknap](https://www.ian-belkap.com)
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return <div className="App">hello world</div>;
+}
+
+export default App;
