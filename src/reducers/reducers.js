4e43320bf87d9e2fd61d60297bcbc8d4ee8ea2ee
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-12-08 13:07:45 -0500

    Began setting up initial state in reducers.js.

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,9 +1,29 @@
+import boardMaker from '../game_functions/boardMaker';
+import randomSandwichPlacer from '../game_functions/randomSandwichPlacer';
+
+const blankGrid = boardMaker(10);
+const coordsArray = randomSandwichPlacer();
+
+// grab the coordinates and map them to the grid
+const coords = coordsArray.map((c) => c.slice(3));
+const updateGrid = blankGrid.map((s) => {
+  if (coords.includes(s.id)) {
+    return { ...s, open: false };
+  }
+  return s;
+});
+
 let initialState = JSON.parse(localStorage.getItem('appState'));
 
 if (!initialState) {
   initialState = {
-    someData: [],
-    someOtherData: [],
+    view: 'showTop',
+    iso: 'flat',
+    grid: updateGrid,
+    coords,
+    allGingham: ['green', 'red', 'black', 'blue'],
+    myGingham: 'blue',
+    gameGingham: 'red',
   };
 }
 
