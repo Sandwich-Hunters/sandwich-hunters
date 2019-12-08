3c0f392c954b624fcccc693710b5089468d4c7ac
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-12-08 13:48:07 -0500

    Added UPDATE_GRID action to reducers.

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -29,32 +29,13 @@ if (!initialState) {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD_CARD': {
-      const tempCards = [...state.cards, { ...action.payload }];
-      return { ...state, cards: tempCards };
+    case 'UPDATE_GRID': {
+      const tempGrid = [...state.grid];
+      const { index } = action.payload;
+      tempGrid[index].active = true;
+      return { ...state, grid: tempGrid };
     }
-    case 'MOVE_CARD_LEFT': {
-      const tempCards = [...state.cards].map((card) => {
-        const tempCard = { ...card };
-        if (tempCard.id === action.payload.id) {
-          if (tempCard.colNum === 0) tempCard.colNum = state.columns.length - 1;
-          else tempCard.colNum -= 1;
-        }
-        return tempCard;
-      });
-      return { ...state, cards: tempCards };
-    }
-    case 'MOVE_CARD_RIGHT': {
-      const tempCards = [...state.cards].map((card) => {
-        const tempCard = { ...card };
-        if (tempCard.id === action.payload.id) {
-          if (tempCard.colNum === state.columns.length - 1) tempCard.colNum = 0;
-          else tempCard.colNum += 1;
-        }
-        return tempCard;
-      });
-      return { ...state, cards: tempCards };
-    }
+
     default:
       return state;
   }
