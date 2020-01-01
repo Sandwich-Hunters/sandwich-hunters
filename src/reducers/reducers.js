fc70f3f020a9ee576841eeef6f764aba297280ce
Ian Be <ian84be@gmail.com>
2020-01-01 13:31:01 -0600

    remove dead code

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,14 +1,5 @@
 import boardMaker from '../game_functions/boardMaker';
 
-// grab the coordinates and map them to the grid
-// const coords = coordsArray.map((c) => c.slice(3));
-// const updateGrid = blankGrid.map((s) => {
-//   if (coords.includes(s.id)) {
-//     return { ...s, open: false };
-//   }
-//   return s;
-// });
-
 let initialState = JSON.parse(localStorage.getItem('appState'));
 
 if (!initialState) {
