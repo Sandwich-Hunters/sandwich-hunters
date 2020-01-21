97faafb3bb461e8becc73961953d96047e88662d
Ian Be <ian84be@gmail.com>
2020-01-20 19:01:09 -0600

    refactor MySammies display component

--- src/components/MySammies/MySammies.jsx
+++ src/components/MySammies/MySammies.jsx
@@ -5,31 +5,58 @@ import '../../scss/MySammies.scss';
 export default function MySammies() {
   const playerGingham = useSelector((state) => state.playerGingham);
   return (
-    <div className="MySammies">
-      <section className={`MySammies__body ${playerGingham}`}>
-        <header>My Sammies</header>
-        <div className="MySammies__sammy bread wedge left" />
-        <div className="MySammies__sammy bread wedge right" />
-        <div className="MySammies__sammy wedge label">wedge</div>
-        <div className="MySammies__sammy bread sub left" />
-        <div className="MySammies__sammy bread sub mid" />
-        <div className="MySammies__sammy bread sub right" />
-        <div className="MySammies__sammy sub label">submarine</div>
-        <div className="MySammies__sammy bread hoagie left" />
-        <div className="MySammies__sammy bread hoagie mid" />
-        <div className="MySammies__sammy bread hoagie right" />
-        <div className="MySammies__sammy hoagie label">hoagie</div>
-        <div className="MySammies__sammy bread grinder left" />
-        <div className="MySammies__sammy bread grinder mid-1" />
-        <div className="MySammies__sammy bread grinder mid-2" />
-        <div className="MySammies__sammy bread grinder right" />
-        <div className="MySammies__sammy grinder label">grinder</div>
-        <div className="MySammies__sammy bread hero left" />
-        <div className="MySammies__sammy bread hero mid-1" />
-        <div className="MySammies__sammy bread hero mid-2" />
-        <div className="MySammies__sammy bread hero mid-3" />
-        <div className="MySammies__sammy bread hero right" />
-        <div className="MySammies__sammy hero label">hero</div>
+    <div className={`MySammies ${playerGingham}`}>
+      <header>My Sammies</header>
+
+      <section className="MySammies__body">
+
+        <section className="MySammies__sammy__container">
+          <div className="MySammies__sammy--draggable">
+            <div className="MySammies__sammy bread left" />
+            <div className="MySammies__sammy bread right" />
+          </div>
+          <div className="MySammies__sammy label">wedge</div>
+        </section>
+
+        <section className="MySammies__sammy__container">
+          <div className="MySammies__sammy--draggable">
+            <div className="MySammies__sammy bread left" />
+            <div className="MySammies__sammy bread mid" />
+            <div className="MySammies__sammy bread right" />
+          </div>
+          <div className="MySammies__sammy label">submarine</div>
+        </section>
+
+        <section className="MySammies__sammy__container">
+          <div className="MySammies__sammy--draggable">
+            <div className="MySammies__sammy bread left" />
+            <div className="MySammies__sammy bread mid" />
+            <div className="MySammies__sammy bread right" />
+          </div>
+          <div className="MySammies__sammy label">hoagie</div>
+        </section>
+
+        <section className="MySammies__sammy__container">
+          <div className="MySammies__sammy--draggable">
+            <div className="MySammies__sammy bread left" />
+            <div className="MySammies__sammy bread mid" />
+            <div className="MySammies__sammy bread mid" />
+            <div className="MySammies__sammy bread right" />
+          </div>
+          <div className="MySammies__sammy label">grinder</div>
+        </section>
+
+        <section className="MySammies__sammy__container">
+          <div className="MySammies__sammy--draggable">
+            <div className="MySammies__sammy bread left" />
+            <div className="MySammies__sammy bread mid" />
+            <div className="MySammies__sammy bread mid" />
+            <div className="MySammies__sammy bread mid" />
+            <div className="MySammies__sammy bread right" />
+          </div>
+          <div className="MySammies__sammy label">hero</div>
+        </section>
+
       </section>
     </div>
   );
